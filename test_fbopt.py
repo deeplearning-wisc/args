17f46c8c6234eeef59866d3bffa6369f5dfff13f
maximkha <maximkhanov.ku@gmail.com>
2023-06-07 20:19:10 -0500

    facebook opt + cleaned

---
+++ test_fbopt.py
@@ -0,0 +1,15 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+model = AutoModelForCausalLM.from_pretrained("facebook/opt-6.7b", torch_dtype=torch.float16).cuda()
+tokenizer = AutoTokenizer.from_pretrained("facebook/opt-6.7b", use_fast=False)
+
+def run_model(prompt: str) -> str:
+    global model, tokenizer
+    tokens = tokenizer(prompt, return_tensors="pt").input_ids.cuda()
+    res_tokens = model.generate(tokens, max_new_tokens=20)
+    res_text = tokenizer.batch_decode(res_tokens, skip_special_tokens=True)
+    return res_text[0]
+
+while True:
+    print(run_model(input("> ")))
