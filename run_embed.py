800675102c00a5ea0cf9f8c218d975c74a9cd29f
maximkha <maximkhanov.ku@gmail.com>
2023-06-13 00:05:02 -0500

    Embedding now works and definitely doesn't have a memory leak

--- run_embed.py
+++ run_embed.py
@@ -1,46 +1,56 @@
-from torch import nn
-from torch import optim
-from llm_features import EmbeddedHHRLHF, LLaMaEmbeddingGenerator
+from llm_features import LLaMaEmbeddingGenerator, OPTEmbeddingGenerator
 import gc
 import torch
+from datasets import load_dataset
+from tqdm import tqdm
+from pathlib import Path
 
-ds = EmbeddedHHRLHF(["pooling_mode_mean_tokens"], [16], llm_embedding=LLaMaEmbeddingGenerator)
+CUDA_DEVICE = 0
+BATCH_SIZE = 16
 
-ds.llm_embedding_generator.llm = ds.llm_embedding_generator.llm.cuda()
+DATASET = "Anthropic/hh-rlhf"
+SPLIT = "train"
+HAS_SFT = False
+POOLING_METHODS = ["pooling_mode_mean_tokens"]
+LAYERS = [-1]
 
-# auxmodel = nn.Sequential(nn.Linear(4096, 2048), nn.ReLU(), 
-#                          nn.Linear(2048, 1024), nn.ReLU(), 
-#                          nn.Linear(1024, 512), nn.ReLU(), 
-#                          nn.Linear(512, 256), nn.ReLU(),
-#                          nn.Linear(256, 1))
+pooling_method_str = str(list(map(lambda x: x[len("pooling_mode_"):], POOLING_METHODS))).replace('\'','').replace(' ','')
+layer_str = str(LAYERS).replace('\'','').replace(' ','')
+res_dir_name = f"{DATASET.replace('/', '_').replace('-', '_')}_{SPLIT}_{'sft' if HAS_SFT else 'nosft'}_{layer_str}_{pooling_method_str}"
 
-auxmodel = nn.Sequential(nn.Linear(4096, 1024), nn.ReLU(), 
-                         nn.Linear(1024, 256), nn.ReLU(), 
-                         nn.Linear(256, 1)).cuda()
+current_dir = Path(".")
+res_dir = current_dir / "processed" / res_dir_name
 
-# , nn.Sigmoid()
+print(f"{res_dir=}")
+embedding_gen = LLaMaEmbeddingGenerator(POOLING_METHODS, LAYERS)
+embedding_gen.llm = embedding_gen.llm.cuda(CUDA_DEVICE)
 
-loss_f = nn.BCEWithLogitsLoss()
-optimizer = optim.Adam(auxmodel.parameters())
+dataset = load_dataset(DATASET, data_dir=None)[SPLIT]
 
-while True:
+def embed(row):
+    print("running embed")
+    global embedding_gen
+    all_text = row["chosen"] + row["rejected"]
     with torch.no_grad():
-        batch = ds.next()
-    
-    if batch is None: break
+        embeddings_gpu = embedding_gen.process_batched_text(all_text)
 
-    optimizer.zero_grad()
+        labels = torch.zeros(len(all_text)).cpu()
+        labels[0:len(row["chosen"])] = 1
 
-    model_out = auxmodel(batch["embeddings"]).cuda()
-    expected = batch["labels"].view(-1, 1).cuda()
-
-    loss_value = loss_f(model_out, expected)
-
-    print(f"{loss_value.item()=}")
-    loss_value.backward()
-    optimizer.step()
-
-    # reclaim memory from batch
-    del batch
+    print("moving to cpu")
+    embeddings = embeddings_gpu.cpu()
+    del embeddings_gpu
+    print("collecting")
     gc.collect()
     torch.cuda.empty_cache()
+    print("done")
+    return {"embeddings": embeddings, "labels": labels}
+
+res_dir.mkdir(parents=True, exist_ok=True)
+
+# for row in tqdm(dataset.iter(batch_size=BATCH_SIZE)):
+#     embed(row)
+
+new_ds = dataset.map(embed, batched=True, batch_size=BATCH_SIZE, remove_columns=["rejected", "chosen"])
+
+new_ds.save_to_disk(res_dir.absolute())
